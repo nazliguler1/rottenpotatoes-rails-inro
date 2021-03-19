9ececc4618ab5c52e33d447ccfc74257705ddcdc
Nazli Guler <nguler1@binghamton.edu>
2021-03-19 15:21:18 +0000

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  root 'movies#index'
+  
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -15,6 +14,8 @@ Rails.application.routes.draw do
   #   resources :products
 
   resources :movies
+  # You can have the root of your site routed with "root"
+  root 'movies#index'
   
   # Example resource route with options:
   #   resources :products do
